6d75bd691e7989571d7031d3fe2a187f22a5f0c6
Keshav Pareek <pareekk129@gmail.com>
2025-07-06 23:19:02 +0530

    Time: 13 ms (88.20%) | Memory: 58.3 MB (54.15%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-
+/*
     class Solution {
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
@@ -22,4 +22,16 @@ import java.util.Arrays;
     }return false;
 }
 }*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
     
