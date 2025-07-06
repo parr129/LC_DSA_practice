85e9b61f9d69fbb9d9f497fec6cdeb68251b1152
Keshav Pareek <pareekk129@gmail.com>
2025-07-06 23:18:59 +0530

    Time: 13 ms (88.2%), Space: 58.3 MB (54.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+/*
+    class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+    /*for(int i = 0; i < nums.length; i++){
+        for(int j = 0; j < nums.length; j++){
+            if( nums[i] == nums [j]){
+               return true;
+  //brute force
+                
+            }
+        }
+    }return false;
+}
+}*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
+    
