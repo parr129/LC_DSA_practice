94cf412b8e0ee58dde9a890019305ddae22d2907
Keshav Pareek <pareekk129@gmail.com>
2025-08-12 11:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (88.98%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,7 +9,8 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-            if (slow == fast) return true;
+            if (slow == fast){
+            return true;}
         }
 
         return false;
