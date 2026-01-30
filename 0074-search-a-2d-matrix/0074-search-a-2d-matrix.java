c746229c2049cce1ae548b412cf7f9a79f74c21f
Keshav <pareekk129@gmail.com>
2026-01-30 23:53:25 +0530

    Time: 0 ms (100%), Space: 44.1 MB (35.9%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,63 +1,12 @@
-/*public class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int low = 0, high = m * n - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            int row = mid / n;
-            int col = mid % n;
-            int midVal = matrix[row][col];
-
-            if (midVal == target) {
-                return true;
-            } else if (midVal < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return false;
-    }
-}*/
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int top = 0;
-        int bot = matrix.length - 1;
-
-        while (top <= bot) {
-            int mid = (top + bot) / 2;
-
-            if (matrix[mid][0] < target && matrix[mid][matrix[mid].length - 1] > target) {
-                break;
-            } else if (matrix[mid][0] > target) {
-                bot = mid - 1;
-            } else {
-                top = mid + 1;
-            }
+    public boolean searchMatrix(int[][] m,int t){
+        int r=m.length,c=m[0].length,l=0,h=r*c-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            int v=m[mid/c][mid%c];
+            if(v==t) return true;
+            if(v<t) l=mid+1; else h=mid-1;
         }
-
-        int row = (top + bot) / 2;
-
-        int left = 0;
-        int right = matrix[row].length - 1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (matrix[row][mid] == target) {
-                return true;
-            } else if (matrix[row][mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return false;        
+        return false;
     }
 }
