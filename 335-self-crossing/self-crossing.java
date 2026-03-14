8998595ccf416c7b220f1faa51fe6c57e0e2641b
Keshav <pareekk129@gmail.com>
2026-03-14 20:39:58 +0530

    Time: 8 ms (43.70%) | Memory: 59.4 MB (38.77%) - LeetSync

---
+++ 335-self-crossing/self-crossing.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSelfCrossing(int[] d) {
+        for (int i = 3; i < d.length; i++) {
+
+            if (d[i] >= d[i-2] && d[i-1] <= d[i-3]) return true;
+
+            if (i >= 4 && d[i-1] == d[i-3] && d[i] + d[i-4] >= d[i-2])
+                return true;
+
+            if (i >= 5 &&
+                d[i-2] >= d[i-4] &&
+                d[i] >= d[i-2] - d[i-4] &&
+                d[i-1] >= d[i-3] - d[i-5] &&
+                d[i-1] <= d[i-3])
+                return true;
+        }
+        return false;
+    }
+}
