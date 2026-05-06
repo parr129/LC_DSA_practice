65cc27268c39121186209a186cb0d2553c67947c
Keshav <pareekk129@gmail.com>
2026-05-06 10:29:07 +0530

    Time: 7 ms (93.03%) | Memory: 124.9 MB (28.12%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,21 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length, n = box[0].length;
+        for (int i = 0; i < m; i++) {
+            int k = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') k = j - 1;
+                else if (box[i][j] == '#') {
+                    char t = box[i][j];
+                    box[i][j] = '.';
+                    box[i][k--] = t;
+                }
+            }
+        }
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                res[j][m - 1 - i] = box[i][j];
+        return res;
+    }
+}
