72cdfb42482c8e0bab5801a154bbd4a61859d1d7
Keshav <pareekk129@gmail.com>
2026-05-14 11:31:49 +0530

    Time: 5 ms (48.38%), Space: 45.1 MB (43.05%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != i + 1) return false;
+        }
+
+        return nums[n - 1] == n - 1;
+    }
+}
