12553b75c9dbcb89c20ba134021c90ff623f4651
Keshav <pareekk129@gmail.com>
2026-05-24 17:55:43 +0530

    Time: 10 ms (87.43%), Space: 46.5 MB (30.29%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.java
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -1,24 +1,37 @@
-import java.util.*;
 class Solution {
-    public int maxJumps(int[] a, int d) {
-        int n=a.length;
-        Integer[] idx=new Integer[n];
-        for(int i=0;i<n;i++)idx[i]=i;
-        Arrays.sort(idx,(x,y)->a[x]-a[y]);
-        int[] dp=new int[n];
-        int r=1;
-        for(int j:idx){
-            dp[j]=1;
-            for(int k=j-1;k>=Math.max(0,j-d);k--){
-                if(a[k]>a[j])break;
-                dp[j]=Math.max(dp[j],dp[k]+1);
-            }
-            for(int k=j+1;k<=Math.min(n-1,j+d);k++){
-                if(a[k]>a[j])break;
-                dp[j]=Math.max(dp[j],dp[k]+1);
-            }
-            r=Math.max(r,dp[j]);
+    public int maxJumps(int[] arr, int d) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfs(arr, d, i, dp));
         }
-        return r;
+
+        return ans;
+    }
+
+    private int dfs(int[] arr, int d, int i, int[] dp) {
+        if (dp[i] != 0) {
+            return dp[i];
+        }
+
+        int max = 1;
+        for (int j = i - 1; j >= Math.max(0, i - d); j--) {
+            if (arr[j] >= arr[i]) {
+                break;
+            }
+            max = Math.max(max, 1 + dfs(arr, d, j, dp));
+        }
+
+       
+        for (int j = i + 1; j <= Math.min(arr.length - 1, i + d); j++) {
+            if (arr[j] >= arr[i]) {
+                break;
+            }
+            max = Math.max(max, 1 + dfs(arr, d, j, dp));
+        }
+
+        return dp[i] = max;
     }
 }
